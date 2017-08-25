bb72088071789b64bfbaf0e58282016923391e2e
Greg Dardis <dardis.greg@gmail.com>
2017-08-25 11:39:34 -0600

    very sloppy implementation of generating board using loops

--- src/Board.js
+++ src/Board.js
@@ -13,22 +13,31 @@ export class Board extends React.Component {
   }
 
   render() {
+    var gridTopRow = [];
+    for (var i = 0; i < 3; i++) {
+      gridTopRow.push(this.renderSquare(i));
+    }
+
+    var gridMiddleRow = [];
+    for (var i = 3; i < 6; i++) {
+      gridMiddleRow.push(this.renderSquare(i));
+    }
+
+    var gridBottomRow = [];
+    for (var i = 6; i < 9; i++) {
+      gridBottomRow.push(this.renderSquare(i));
+    }
+
     return (
       <div>
         <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
+          {gridTopRow}
         </div>
         <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
+          {gridMiddleRow}
         </div>
         <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
+          {gridBottomRow}
         </div>
       </div>
     );
